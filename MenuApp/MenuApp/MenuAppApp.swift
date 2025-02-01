b6a9fe7dcadaf603dd99c213718f1e938a5e9cde
luran1 <luranmanfio0701@gmail.com>
2025-02-01 13:12:26 -0500

    Testing git commits.

---
+++ MenuApp/MenuApp/MenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppApp.swift
+//  MenuApp
+//
+//  Created by Madison Francis on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
