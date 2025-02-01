3ee51d2671ca31dc02606693f42e3a64c18d9e84
luran1 <luranmanfio0701@gmail.com>
2025-02-01 16:03:13 -0500

    basic list of objects that have a name, description, and an image. these items are grouped by headers and have a navigationLink with value set to item.

--- MenuApp/MenuApp/ContentView.swift
+++ MenuApp/MenuApp/ContentView.swift
@@ -9,10 +9,75 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Testing commits on experimental branch.")
+        NavigationStack{
+            List{
+                Section{
+                    ForEach(1..<20, id: \.self){ item in
+                        HStack(spacing: 15){
+                            Image(systemName: "plus")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height:80)
+                                .cornerRadius(4)
+                            VStack(alignment: .leading,spacing: 5){
+                                Text("Item Name")
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                    .lineLimit(2)
+                                    .minimumScaleFactor(0.8)
+                                
+                                Text("Item Description")
+                                    .font(.subheadline)
+                                    .foregroundStyle(.secondary)
+                                    .lineLimit(1)
+                                    .minimumScaleFactor(0.9)
+                            }
+                        }
+                    }
+                } header: {
+                    Text("Entres")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.primary)
+                }
+                Section{
+                    ForEach(1..<20, id: \.self){ item in
+                        NavigationLink(value: item) {
+                            HStack(spacing: 15){
+                                Image(systemName: "plus")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height:80)
+                                    .cornerRadius(4)
+                                VStack(alignment: .leading,spacing: 5){
+                                    Text("Item Name")
+                                        .font(.headline)
+                                        .fontWeight(.bold)
+                                        .lineLimit(2)
+                                        .minimumScaleFactor(0.8)
+                                    
+                                    Text("Item Description")
+                                        .font(.subheadline)
+                                        .foregroundStyle(.secondary)
+                                        .lineLimit(1)
+                                        .minimumScaleFactor(0.9)
+                                }
+                            }
+                        }
+                    }
+                } header: {
+                    Text("Sides")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.primary)
+                }
+                
+            }
+            .listStyle(.plain)
+            .navigationTitle("Resturant Name")
         }
-        .padding()
+        
+        
     }
 }
 
